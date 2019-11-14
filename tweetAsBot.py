8c752133d3662c22205a313f31c5fc0c90c27fde
eS-1 <cybergun00@gmail.com>
2019-11-14 14:05:08 +0900

    a few fixes

--- tweetAsBot.py
+++ tweetAsBot.py
@@ -1,4 +1,5 @@
 from config import myAPI
+import tweepy
 from datetime import datetime
 from datetime import timedelta
 import random
@@ -13,5 +14,25 @@ def TweetAsBot():
     myAPI.update_status(tweet)
 
 
+def GetAllFollowers():
+    followers = list()
+    for follower in tweepy.Cursor(myAPI.followers).items():
+        followers.append(follower)
+    return followers
+
+
+def GetAllFriends():
+    friends = list()
+    for friend in tweepy.Cursor(myAPI.friends).items():
+        friends.append(friend)
+    return friends
+
+
 if __name__ == "__main__":
     TweetAsBot()
+
+    followers = GetAllFollowers()
+    friends = GetAllFriends()
+    for unknown in followers:
+        if unknown not in friends:
+            unknown.follow()
