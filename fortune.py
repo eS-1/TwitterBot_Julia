c6fe609240aaab28b8cf18a83f8b469c57acddeb
eS-1 <cybergun00@gmail.com>
2020-02-03 20:08:40 +0900

    some changes in fortune

--- fortune.py
+++ fortune.py
@@ -5,5 +5,15 @@ def draw_fortune():
     '''
     おみくじ機能
     '''
-    deck = ["大吉", "中吉", "小吉", "吉", "末吉"]
-    return random.choice(deck)
+    result = "どれどれ、結果は..."
+    deck = ["大吉",
+            "中吉",
+            "小吉",
+            "吉",
+            "末吉"]
+    result += random.choice(deck) + "だ。ついでに1曲聴いてくか？"
+    return result
+
+
+if __name__ == "__main__":
+    print(draw_fortune())
